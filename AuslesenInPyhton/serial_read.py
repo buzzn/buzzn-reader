de1b55b1484cb1e6982bc52257953c4f685574ae
mustafa akman <mustafaakman@ymail.com>
2016-03-14 18:12:32 +0100

    Kommentar hinzugefügt

--- AuslesenInPyhton/serial_read.py
+++ AuslesenInPyhton/serial_read.py
@@ -1,13 +1,16 @@
 import time
 import serial
-
+'''
+Das Programm liest den String der vom Stromzähler ankommt und druckt es gleich mal aus.
+Als Nächstes werde ich mir anschauen wie genau das mit dem redis Datenbank funktioniert.
+'''
 ser = serial.Serial(
       port = '/dev/ttyAMA0',
-      baudrate = 9600, 
-      parity =serial.PARITY_NONE, 
-      stopbits=serial.STOPBITS_ONE, 
+      baudrate = 9600,
+      parity =serial.PARITY_NONE,
+      stopbits=serial.STOPBITS_ONE,
       bytesize=serial.SEVENBITS,
-      timeout = 1, 
+      timeout = 1,
       )
 
 while 1:
