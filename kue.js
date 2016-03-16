ce2639f20ff7b52ddb92bddbd73cb5e484e86a11
Felix Faerber <ffaerber@gmail.com>
2016-03-16 10:07:00 +0100

    working parser in kue

--- kue.js
+++ kue.js
@@ -7,14 +7,34 @@ var redisClient = redis.createClient();
 
 jobs.process('reading', function(job, done) {
   var reading = job.data.reading;
+  var regex = /([0-9-:.]+)\*(?:[0-9]+)\((.*)\)/g;
+  var m;
+  var obis = {};
+  while ((m = regex.exec(reading))) {
+    obis[m[1]] = m[2];
+  }
 
-  redisClient.get('token', function(err, token) {
-    rest.post('http://localhost:3001/api/v1/readings',{
-      accessToken: token,
-      data: {
-        reading: reading
-      }
-    });
-  });
+  var meterNummer = obis['0:0.0.0'];
+  var inWh        = obis['1-0:1.8.1']*10000;
+  var outWh       = obis['1-0:2.8.1']*10000;
+
+  console.log(inWh);
+  console.log(outWh);
+
+  // redisClient.mget(['token', 'metering_point_id'], function(err, reply) {
+  //   rest.post('https://staging.buzzn.net/api/v1/readings',{
+  //     accessToken: reply[0],
+  //     data: {
+  //       metering_point_id: reply[1],
+  //       timestamp: Date(job.created_at*1000)
+  //     },
+  //   }).on('success', function(data, response) {
+  //     console.log('success')
+  //   }).on('fail', function(data, response) {
+  //     console.log("Fail:", data )
+  //   }).on('error', function(err, response) {
+  //     console.log('Error:', err);
+  //   });
+  // });
   done();
 });
