e0d354a0dfc82f62af002188e501419832812e3b
Felix Faerber <ffaerber@gmail.com>
2016-03-18 17:47:45 +0100

    add kue to serialport reader

--- AuslesenInNode.js
+++ AuslesenInNode.js
@@ -1,11 +1,16 @@
+var kue = require('kue')
+, queue = kue.createQueue();
+
 var SerialPort = require("serialport").SerialPort
 var serialPort = new SerialPort("/dev/ttyAMA0", {
   baudrate: 9600, dataBits: 7, stopBits: 1, bufferSize : 700
-}); // this is the openImmediately flag [default is true] 
+}); // this is the openImmediately flag [default is true]
 
 var Auslese = ""
 serialPort.on('data', function(data) {
-    console.log(data.toString());}
+  console.log(data.toString());
+  queue.create('reading', { reading: data.toString() }).save();
+}
 );
 
 ////////serialPort.open(function (error) {
