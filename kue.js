4378fea77db057675b471c0e18e4f4071eb630ea
Felix Faerber <ffaerber@gmail.com>
2016-04-30 14:12:16 +0200

    tests

--- kue.js
+++ kue.js
@@ -6,23 +6,28 @@ var redisClient = redis.createClient();
 
 jobs.process('reading', function(job, done) {
   var smlParser = new SmlParser(job.data.sml);
-  redisClient.mget(['token'], function(err, reply) {
-    rest.post('https://staging.buzzn.net/api/v1/readings',{
-      accessToken: reply[0],
-      data: {
-        timestamp: Date(job.created_at*1000),
-        meter_serialnumber: smlParser.meterSerialnumber,
-        watt_hour_a: smlParser.wattHourA,
-        watt_hour_b: smlParser.wattHourB,
-        power: smlParser.power
-      },
-    }).on('success', function(data, response) {
-      console.log('success')
-    }).on('fail', function(data, response) {
-      console.log("Fail:", data )
-    }).on('error', function(err, response) {
-      console.log('Error:', err);
-    });
-  });
+
+  console.log("power: " + smlParser.power)
+  console.log("wattHourA: " + smlParser.wattHourA)
+  console.log("wattHourB: " + smlParser.wattHourB)
+
+  // redisClient.mget(['token'], function(err, reply) {
+  //   rest.post('https://staging.buzzn.net/api/v1/readings',{
+  //     accessToken: reply[0],
+  //     data: {
+  //       timestamp: Date(job.created_at*1000),
+  //       meter_serialnumber: smlParser.meterSerialnumber,
+  //       watt_hour_a: smlParser.wattHourA,
+  //       watt_hour_b: smlParser.wattHourB,
+  //       power: smlParser.power
+  //     },
+  //   }).on('success', function(data, response) {
+  //     console.log('success')
+  //   }).on('fail', function(data, response) {
+  //     console.log("Fail:", data )
+  //   }).on('error', function(err, response) {
+  //     console.log('Error:', err);
+  //   });
+  // });
   done();
 });
