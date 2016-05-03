a055abbfa814f86d227f786b82ee1634e776c84f
Felix Faerber <ffaerber@gmail.com>
2016-05-03 17:40:58 +0200

    test

--- kue.js
+++ kue.js
@@ -6,22 +6,24 @@ var redisClient = redis.createClient();
 var SmlParser = require('./libs/sml_parser');
 
 jobs.process('sml', function(job, done) {
-  var smlParser = new SmlParser(job.data.sml);
-  redisClient.mget(['token'], function(err, reply) {
-    rest.post('https://staging.buzzn.net/api/v1/readings',{
-      accessToken: reply[0],
-      data: {
-        timestamp: Date(job.created_at*1000),
-        watt_hour: smlParser.wattHourA,
-        power: smlParser.power
-      },
-    }).on('success', function(data, response) {
-      console.log('success')
-    }).on('fail', function(data, response) {
-      console.log("Fail:", data )
-    }).on('error', function(err, response) {
-      console.log('Error:', err);
-    });
-  });
+  console.log(job.data.sml)
+
+  // var smlParser = new SmlParser(job.data.sml);
+  // redisClient.mget(['token'], function(err, reply) {
+  //   rest.post('https://staging.buzzn.net/api/v1/readings',{
+  //     accessToken: reply[0],
+  //     data: {
+  //       timestamp: Date(job.created_at*1000),
+  //       watt_hour: smlParser.wattHourA,
+  //       power: smlParser.power
+  //     },
+  //   }).on('success', function(data, response) {
+  //     console.log('success')
+  //   }).on('fail', function(data, response) {
+  //     console.log("Fail:", data )
+  //   }).on('error', function(err, response) {
+  //     console.log('Error:', err);
+  //   });
+  // });
   done();
 });
