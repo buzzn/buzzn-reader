93c334c47fb11a473cbc23afe0a5c3f8c87a18cd
Felix Faerber <ffaerber@gmail.com>
2016-05-03 17:40:01 +0200

    remove junk

--- routes/index.js
+++ routes/index.js
@@ -16,15 +16,4 @@ router.get('/', function(req, res, next) {
   });
 });
 
-router.get('/new_job', function(req, res) {
-  res.render('jobs_new');
-});
-
-router.post('/create_job', function(req, res) {
-  var reading  = req.body.txt;
-  queue.create('reading', { reading: reading }).save();
-  res.redirect("new_job");
-});
-
-
 module.exports = router;
