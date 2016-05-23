ea4a3457837987bff877c1aa8533886b23b762f4
Felix Faerber <ffaerber@gmail.com>
2016-05-23 20:18:39 +0200

    aktivate sml sending

--- kue.js
+++ kue.js
@@ -7,27 +7,22 @@ var SmlParser = require('./libs/sml_parser');
 
 jobs.process('sml', function(job, done) {
   var smlParser = new SmlParser(job.data.sml);
-  console.log(smlParser.meterSerialnumber);
-  console.log(smlParser.energyAMilliwattHour);
-  console.log(smlParser.energyBMilliwattHour);
-  console.log(smlParser.powerMilliwatt);
-
-  // var smlParser = new SmlParser(job.data.sml);
-  // redisClient.mget(['token'], function(err, reply) {
-  //   rest.post('https://staging.buzzn.net/api/v1/readings',{
-  //     accessToken: reply[0],
-  //     data: {
-  //       timestamp: Date(job.created_at*1000),
-  //       watt_hour: smlParser.wattHourA,
-  //       power: smlParser.power
-  //     },
-  //   }).on('success', function(data, response) {
-  //     console.log('success')
-  //   }).on('fail', function(data, response) {
-  //     console.log("Fail:", data )
-  //   }).on('error', function(err, response) {
-  //     console.log('Error:', err);
-  //   });
-  // });
-  done();
+  redisClient.mget(['host', 'token'], function(err, reply) {
+    rest.post(reply[0],{
+      accessToken: reply[1],
+      data: {
+        timestamp: Date(job.created_at),
+        meter_serialnumber: smlParser.meterSerialnumber,
+        energy_a_milliwattHour: smlParser.energyAMilliwattHour,
+        energy_b_milliwattHour: smlParser.energyBMilliwattHour,
+        power_milliwatt: smlParser.powerMilliwatt
+      },
+    }).on('success', function(data, response) {
+      done();
+    }).on('fail', function(data, response) {
+      console.log("Fail:", data )
+    }).on('error', function(err, response) {
+      console.log('Error:', err);
+    });
+  });
 });
