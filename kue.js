f84005f7154711728fc8b9a0da426898a636426d
Felix Faerber <ffaerber@gmail.com>
2016-07-10 23:31:00 +0200

    redis host

--- kue.js
+++ kue.js
@@ -2,7 +2,8 @@ var kue = require('kue');
 var jobs = kue.createQueue();
 var rest = require('restler');
 var redis = require('redis');
-var redisClient = redis.createClient(6379,'redis');
+var redisClient = redis.createClient({ host: 'redis', port: 6379 });
+
 var SmlParser = require('./libs/sml_parser');
 
 jobs.process('sml', function(job, done) {
--- routes/settings.js
+++ routes/settings.js
@@ -3,7 +3,7 @@ var router = express.Router();
 var rest = require('restler');
 
 var redis = require('redis');
-var redisClient = redis.createClient(6379, 'redis')
+var redisClient = redis.createClient({ host: 'redis', port: 6379 });
 
 router.get('/edit', function(req, res) {
   redisClient.mget(['host', 'token'], function(err, reply) {
