5a77a307d9966e74f696e5703c991e274cc92993
Felix Faerber <ffaerber@gmail.com>
2016-10-13 13:58:23 +0200

    new serialport dsl

--- serialport.js
+++ serialport.js
@@ -1,23 +1,26 @@
-// const config = require('config')
+'use strict';
+
+const config = require('config')
 const SerialPort = require('serialport')
+const Kue = require('kue')
 
-// const Kue = require('kue')
-// let jobs = Kue.createQueue({
-//     redis: {
-//         host: config.get('redis.host')
-//     }
-// })
+let jobs = Kue.createQueue({
+    redis: {
+        host: config.get('redis.host')
+    }
+})
 
-let port = new SerialPort('/dev/ttyUSB0')
+let port = new SerialPort('/dev/ttyUSB0', {
+    dataBits: 7
+})
 
-let ReadLine = SerialPort.parsers.ReadLine;
-let parser = port.pipe(ReadLine({
+let readLine = SerialPort.parsers.ReadLine;
+let parser = port.pipe(readLine({
     delimiter: '!'
 }))
 
 parser.on('data', function(data) {
-    console.log(data);
-    // jobs.create('sml', {
-    //     sml: data
-    // }).removeOnComplete(true).save()
+    jobs.create('sml', {
+        sml: data
+    }).removeOnComplete(true).save()
 })
