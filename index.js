36140a8732d138c41329e9b69ed522964bb7b718
Felix Faerber <ffaerber@gmail.com>
2017-04-27 14:17:01 +0200

    reoder

--- index.js
+++ index.js
@@ -3,24 +3,24 @@ require('dotenv').config()
 const SerialPort = require('serialport')
 const mqtt = require('mqtt')
 
+
 const mqttClient = mqtt.connect(process.env.MQTT_BROKER_HOST)
 mqttClient.on('connect', () => {
   mqttClient.subscribe(process.env.MQTT_TOPIC)
   let port = new SerialPort('/dev/ttyUSB0', {dataBits: 7})
   let readLine = SerialPort.parsers.ReadLine;
   let parser = port.pipe(readLine({delimiter: '!'}))
-})
-
-parser.on('data', function(data) {
-  let reading = new Reading(data.sml)
-  if (reading.valid()) {
-    let reading = {
-      meterSerialnumber: meter.meterSerialnumber,
-      energyAMilliwattHour: reading.energyAMilliwattHour,
-      energyBMilliwattHour: reading.energyBMilliwattHour,
-      powerAMilliwatt: reading.powerAMilliwatt,
-      powerBMilliwatt: reading.powerBMilliwatt
+  parser.on('data', function(data) {
+    let reading = new Reading(data.sml)
+    if (reading.valid()) {
+      let reading = {
+        meterSerialnumber: meter.meterSerialnumber,
+        energyAMilliwattHour: reading.energyAMilliwattHour,
+        energyBMilliwattHour: reading.energyBMilliwattHour,
+        powerAMilliwatt: reading.powerAMilliwatt,
+        powerBMilliwatt: reading.powerBMilliwatt
+      }
+      mqttClient.publish(process.env.MQTT_TOPIC, reading.toString())
     }
-    mqttClient.publish(process.env.MQTT_TOPIC, reading.toString())
-  }
+  })
 })
